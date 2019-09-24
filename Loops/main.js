c53abe071edd46697eb2f4da5d70a93ed8da6e5d
AyaanJutt <54960773+AyaanJutt@users.noreply.github.com>
2019-09-24 14:31:16 -0600

    Add files via upload

---
+++ Loops/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+
+for(let happy = 0; happy<500; happy++){
+  console.log('I am so happy!')
+}
+
+
+for(let multiple=12;multiple<=800; multiple+=4 ){
+    console.log(multiple)
+}
+
+
+
+for(let odd=55;odd>=11; odd-=2){
+ console.log(odd)
+}
+
+let sum=0;
+for (let x=5;x<=50;x++){
+    sum+=x
+}
+console.log('Sum is:'+sum)
+
+let sum2=0;
+for (let x2=10;x2<=100;x2++){
+    sum+=x2
+}
+console.log('Sum is:'+sum2)
+
